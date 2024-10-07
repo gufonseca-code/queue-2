5ef273d63a9b87ce2da4483645cc9f95723cbbe1
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2024-10-06 22:45:00 -0300

    First part finished

--- Password Distribution/src/App.java
+++ Password Distribution/src/App.java
@@ -1,5 +1,18 @@
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        PriorityQueue<Patient> queue = new PriorityQueue<>();
+        queue.add(new Patient("Carlos", 1));
+        queue.add(new Patient("Alan", 1));
+        queue.add(new Patient("José", 0));
+        queue.add(new Patient("Bianca", 1));
+        
+        Iterator<Patient> iterator = queue.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(queue.poll());
+        }
     }
 }
