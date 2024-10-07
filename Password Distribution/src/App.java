956ea7360bffd6b92047e18e57a00b88adb8d47b
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2024-10-07 13:15:18 -0300

    Finished test

--- Password Distribution/src/App.java
+++ Password Distribution/src/App.java
@@ -3,16 +3,35 @@ import java.util.PriorityQueue;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        PriorityQueue<Patient> queue = new PriorityQueue<>();
-        queue.add(new Patient("Carlos", 1));
-        queue.add(new Patient("Alan", 1));
-        queue.add(new Patient("José", 0));
-        queue.add(new Patient("Bianca", 1));
-        
-        Iterator<Patient> iterator = queue.iterator();
+        PriorityQueue<Patient> normalQueue = new PriorityQueue<>();
+        PriorityQueue<Patient> priorityQueue = new PriorityQueue<>();
+        String[] names = {"Carlos", "Alan", "José", "Bianca", "Pedro", "Gustavo", "Lucas", "Marcelo", "Luiz", "Maria", "Mariza", "Bruno", "Matheus", "Jeferson", "Felipe", "Vinícius"};
+        int[] priorities = {0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1};
+        int counter = 0;
 
-        while (iterator.hasNext()) {
-            System.out.println(queue.poll());
+        for (int i = 0; i < priorities.length; i++) {
+            if (priorities[i] == 0) {
+                priorityQueue.add(new Patient(names[i], priorities[i]));
+            } else {
+                normalQueue.add(new Patient(names[i], priorities[i]));
+            }
+            
+        }
+        
+        Iterator<Patient> normalIterator = normalQueue.iterator();
+        Iterator<Patient> priorityIterator = priorityQueue.iterator();
+
+        while (priorityIterator.hasNext()) {
+            System.out.println(priorityQueue.poll());
+            counter++;
+
+            if (counter % 3 == 0) {
+                System.out.println(normalQueue.poll());
+            }
+        }
+
+        while (normalIterator.hasNext()) {
+            System.out.println(normalQueue.poll());
         }
     }
 }
